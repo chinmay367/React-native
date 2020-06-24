b4bf3b9ba8501377d7a1018cc077ba01566a7828
Chinmay Choudhary <chchoud@microsoft.com>
2020-06-24 19:52:12 +0530

    updating homescreen file

--- src/Components/MainScreen/HomeScreen.js
+++ src/Components/MainScreen/HomeScreen.js
@@ -1,26 +1,33 @@
-{
-   "uvpName":"ADOBE SYSTEMS INCORPORATE",
-   "document":{
-      "lastModifiedBy":"System",
-      "lastModified":"2020-06-23T13:29:25.5484973Z",
-      "supplierAccountManager":[
-         "wipritch@microsoft.com",
-         "andrews@microsoft.com"
-      ],
-      "executiveSponsor":[
-         "wipritch@microsoft.com",
-         "andrews@microsoft.com"
-      ],
-      "documents":[
-         {
-            "documentType":"orgstructure",
-            "fileName":"supplier.pdf",
-            "lastModifiedBy":"System",
-            "lastModified":"2020-06-23T13:29:25.5484973Z"
-         }
-      ],
-      "createdBy":"System",
-      "created":"2020-06-23T13:29:25.5484973Z"
-   },
-   "documentType":"Relationship"
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, Image, TouchableHighlight } from 'react-native';
+import styles from './styles/styleCard';
+import {recipes} from '../../data/dataArray'
+
+export default function Main(){
+   const onPressRecipe = item => (
+        <Text>add</Text>
+    );
+    
+    const  renderRecipes = ({ item }) => (
+        <TouchableHighlight underlayColor='rgba(73,182,77,1,0.9)' onPress={() => onPressRecipe(item)}>
+          <View style={styles.container}>
+            <Image style={styles.photo} source={{ uri: item.photo_url }} />
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.category}>Category Name</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    
+        return (
+          <View>
+            <FlatList
+              vertical
+              showsVerticalScrollIndicator={false}
+              numColumns={2}
+              data={recipes}
+              renderItem={renderRecipes}
+              keyExtractor={item => `${item.recipeId}`}
+            />
+          </View>
+        );
 }
